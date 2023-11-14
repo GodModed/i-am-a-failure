f4bbfb15ffa042916cb183e8fb4797d5538a8c99
godmoded <djlevy26@gmail.com>
2023-11-14 17:54:26 -0500

    forgot

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+export default {
+    tabWidth: 4,
+    plugins: ['prettier-plugin-astro'],
+    overrides: [
+      {
+        files: '*.astro',
+        options: {
+          parser: 'astro',
+        },
+      },
+    ],
+  };
